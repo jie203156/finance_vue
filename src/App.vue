c5cabd5cd34dd08e44d09a72b4554293427fa8b9
Jie <1850549503@qq.com>
2018-09-28 09:18:24 +0800

    首页部分完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+        <div class="header">
+        <!-- 导航栏 -->
+        <!-- 顶部 -->
+        <div class="topBar">
+            <div class="w">
+                <!-- 搜索部分 -->
+                <div class="search">
+                    <input type="text" placeholder="输入搜索关键内容">
+                    <div class="bigGlass"><a href="#"></a></div>
+                </div>
+                <!-- logo部分 -->
+                <h1 class="logo"><img src="../src/static/images/logo.png" alt=""></h1>
+                <!-- 登录和注册 -->
+                <div class="login">
+                    <a href="#">登录</a>
+                    <i>&nbsp;&nbsp;/&nbsp;&nbsp; </i>
+                    <a href="#">注册</a>
+                </div>
+            </div>
+        </div>
+        <!-- 底部 -->
+        <div class="bottomBar">
+            <div class="content w">
+                <div class="listB">
+                    <ul>
+                        <li><a href="">首页</a></li>
+                        <li><a href="">快讯</a></li>
+                        <li><a href="">项目库</a></li>
+                        <li><a href="">行情</a></li>
+                        <li><a href="">动态</a></li>
+                        <li><a href="">圈子</a></li>
+                        <li><a href="">问答</a></li>
+                        <li><a href="">找伙伴</a></li>
+                        <li><a href="">链氪集结号</a></li>
+                    </ul>
+                </div>
+                <div class="writeArticle"><a href="#">写文章</a></div>
+            </div>
+        </div>
+        <!-- 大图制作 -->
+        <div class="bigPic">
+            
+        </div>
+        <!--小图制作 -->
+        <div class="smallPics w">
+            <ul>
+                <li><a href="#"><img src="" alt=""></a></li>
+                <li><a href="#"><img src="" alt=""></a></li>
+                <li><a href="#"><img src="" alt=""></a></li>
+                <li><a href="#"><img src="" alt=""></a></li>
+            </ul>
+        </div>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './static/css/index.css';
 </style>
